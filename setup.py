ea16dea7b235e73c91ffe42ae126376135139982
David Kopiel <davidko@jfrog.com>
2026-05-06 12:09:33 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='python-sca-test',
+    install_requires=[
+        'fastapi==0.109.0',
+        'lightning==2.0.0',
+        'pypdf==3.1.0',
+        'transformers==4.25.1'
+    ]
+)
